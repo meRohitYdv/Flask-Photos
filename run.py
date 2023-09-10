ad66d4d85f19289a489716545389caef56f18773
Rohit Yadav <rohit.yadav9246@gmail.com>
2023-09-10 11:58:01 +0530

    chore: changes to requirements file

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
-from flaskapp import app
+from flaskapp import app, db
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
     app.run(host="0.0.0.0", port=port)
